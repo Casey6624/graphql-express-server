c8323a8c789c03c64154589324d2d4ebf68b4510
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-04-12 15:04:09 +0100

    First Commit

--- app.js
+++ app.js
@@ -5,16 +5,49 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+const events = []
+
 app.use(bodyParser.json());
 
+/*
+Each type of query needs a variable type.
+[String!]! -- first ! means NOT NULLABLE, value inside the list cannot be null. Second ! is so the list itself cannot return null
+
+It is common practice to use object names like events, just as if events was an object within the application. As opposed to getEvents which
+is more like a function name. This is different for Mutations as mutations are more like function calls with parameter querys 
+
+RootQuery returns an array of Event(s). We define the Event type under Type Event
+*/
+
+app.get("/", (req, res, next) => {
+    res.send("Hello!")
+})
+// http://localhost:4000/graphql for graphiql interface
+
+// ID is not nullable, each event MUST have an ID
 app.use("/graphql", graphqlHttp({
     schema: buildSchema(`
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+
     type RootQuery{
-        events: [String!]!
+        events: [Event!]!
     }
 
     type RootMutation{
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -22,17 +55,25 @@ app.use("/graphql", graphqlHttp({
         mutation: RootMutation
     }
     `),
-    // this is the resolver
+    // this is the resolver. Used to retrieve data
     rootValue: {
         events: () => {
-            return ["Event1", "Event2", "Event3"]
+            return events
         },
+        /* get the event details from args  */
         createEvent: (args) => {
-            const eventName = args.name
-            return eventName
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            }
+            events.push(event)
+            return event
         }
     },
     graphiql: true
 }));
 
-app.listen(3000);
+app.listen(4000);
